4fd8fd6fc53e138a39b3f22e71f6cf025d55c493
JulianoRodrigues07 <juhreolonrodrigues15@gmail.com>
2021-11-19 22:04:36 -0300

    Alteração variavel do historio do game

--- funcoes.py
+++ funcoes.py
@@ -2,5 +2,5 @@ def historico(nome, email):
     nome = input('Insira o seu nome:')
     email = input('Insira o seu e-mail:')
     historico = {'nome':nome,'e-mail':email,}
-    logs = open('logs.txt', 'a')
-    logs.write(str(historico) + '\n')
+    login = open('logs.txt', 'a')
+    login.write(str(historico) + '\n')
